20e6239dead6d7bd16299fcf3bfe3b57bec5acd8
Eder Canada <canada.eder@gmail.com>
2021-12-02 10:25:38 -0800

    add spinner

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Spinner } from 'react-bootstrap';
 
 function App() {
   const [type, setType] = useState('education');
   const [activity, setActivity] = useState('');
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    axios('https://www.boredapi.com/api/activity?type=relaxation').then(
+    setLoading(true);
+    axios(`https://www.boredapi.com/api/activity?type=${type}`).then(
       (response) => {
         setActivity(response.data.activity);
+        setLoading(false);
       }
     );
-  }, [type, activity]);
+  }, [type]);
 
   return (
     <div>
       <button onClick={() => setType('education')}>Education</button>
       <button onClick={() => setType('music')}>Music</button>
       <button onClick={() => setType('relaxation')}>Relaxation</button>
-      <h1>{activity}</h1>
+      {loading ? <Spinner animation="border" /> : <h1>{activity}</h1>}
     </div>
   );
 }
