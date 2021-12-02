01677ad502a0da2dbeaf8b37ccfe2a291815f8f2
Eder Canada <canada.eder@gmail.com>
2021-12-01 21:39:04 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [type, setType] = useState('education');
+  const [activity, setActivity] = useState('');
+
+  useEffect(() => {
+    axios('https://www.boredapi.com/api/activity?type=relaxation').then(
+      (response) => {
+        setActivity(response.data.activity);
+      }
+    );
+  }, [type, activity]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setType('education')}>Education</button>
+      <button onClick={() => setType('music')}>Music</button>
+      <button onClick={() => setType('relaxation')}>Relaxation</button>
+      <h1>{activity}</h1>
     </div>
   );
 }
